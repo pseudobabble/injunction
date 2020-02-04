a029a41122dcbc0b9c6cbc4faa91b689d3f42f96
pseudobabble <harryjohnson500@gmail.com>
2020-02-04 20:21:20 +0000

    return types etc

--- demo.py
+++ demo.py
@@ -7,31 +7,30 @@ from injunction import CommandBus
 
 class HandlerDependency:
 
-    def mutate_output(self, stuff_in: str):
+    def mutate_output(self, stuff_in: str) -> str:
         return stuff_in + ': MUTATED'
 
 
 class Greet(Command):
 
-    def __init__(self, greeting):
+    def __init__(self, greeting: str) -> None:
         self.greeting = greeting
 
-    def get_target(self):
+    def get_target(self) -> CommandHandler:
         return Greeter
 
 
 class Greeter(CommandHandler):
 
-    def __init__(self, handler_dependency=HandlerDependency):
+    def __init__(self, handler_dependency: HandlerDependency = HandlerDependency) -> None:
         self.handler_dependency = handler_dependency()
 
-    def handle(self, command: Greet):
+    def handle(self, command: Greet) -> None:
         greeting = self.handler_dependency.mutate_output(command.greeting)
         print(greeting)
 
 
 if __name__ == '__main__':
-
     hello = Greet('hello there')
 
     CommandBus.issue_synchronous(hello)
